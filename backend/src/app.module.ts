b6a91c1266e04f34977e912425c1b9df91b7ffa3
komqdo <komqdo@MacBook-Pro-de-komqdo.local>
2023-09-15 19:24:51 +0200

    init docker-compose, psql image

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/User';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'app',
+      password: 'apppassword',
+      database: 'transcendance',
+      entities: [ User ],
+      synchronize: false,
+    }),
+  ],
   controllers: [ApiController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
