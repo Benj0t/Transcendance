f173fc4ab56cc66474f8fd93ebfedcf79c295e25
Stanislas Desseaux <stan@airdestanislas.home>
2023-10-26 18:22:55 +0200

    ameliorations jeu, debut ajout packets

--- front/src/components/PongGame.tsx
+++ front/src/components/PongGame.tsx
@@ -3,18 +3,54 @@ import './styles/PongGame.css';
 import Area from './utils/Area';
 import io from 'socket.io-client';
 
-const PongGame: React.FC = () => {
+const PongGame: React.FC = (): any => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const socket = io('http://localhost:8001');
   const [racketY, setRacketY] = useState(0);
+  const [isChoosingMatchmaking, setIsChoosingMatchmaking] = useState(false);
+  const [isChoosingDuel, setIsChoosingDuel] = useState(false);
+  const [isGameStarted, setGameStarted] = useState(false);
+  const [opponentId, setOpponentId] = useState<number | null>(null);
 
+  const sendMatchmakingPacket = (): any => {
+    socket.emit("DualPacket", 0);
+    setGameStarted(true);
+    console.log('Player Y: caca');
+  };
+  
+  const sendDualPacket = (opponentId: number): any => {
+    socket.emit("DualPacket", opponentId);
+    setIsChoosingDuel(true);
+    setGameStarted(true);
+    console.log('Player Y pipi');
+  };
+
+  const sendKeepAlivePacket = (racketY: number): any => {
+    socket.emit("KeeplAlive", racketY);
+    console.log('Player Y: popo');
+  };
+
+  // const sendDualPacket = (): any => {
+  // };
+
+  // const setIdle = (): any => {
+  //   setGameWindow(null);
+  // };
+
+  // const setPlaying = (): any => {
+  // };
+  
+  
   useEffect(() => {
+
     const canvas = canvasRef.current;
-    if (canvas == null) return;
-
+    if (canvas == null)
+      return ;
+  
     const ctx = canvas.getContext('2d');
-    if (ctx == null) return;
-
+    if (ctx == null)
+      return ;
+  
     const area = new Area(canvas.width, canvas.height, 1, 2);
     console.log('Canvas Width:', canvas.width);
     console.log('Canvas Height:', canvas.height);
@@ -23,103 +59,126 @@ const PongGame: React.FC = () => {
       const canvasRect = canvas.getBoundingClientRect();
       const mouseY = e.clientY - canvasRect.top;
       const maxY = canvas.height - area.racketSize().getHeight();
-
-      // Assurez-vous que la position Y est dans les limites du canvas
       const newRacketY = Math.min(maxY, Math.max(0, mouseY));
 
       setRacketY(newRacketY);
+      sendKeepAlivePacket(racketY);
     };
 
-    canvas.addEventListener('mousemove', handleMouseMove);
-    // Fonction de dessin
     const draw = (): void => {
-      // Effacer le contenu du canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      if (isGameStarted)
+      {
+        if (isChoosingMatchmaking) {
+          sendMatchmakingPacket();
+        }
+        else if (isChoosingDuel) {
+          const opponentId = 123;
+          sendDualPacket(opponentId);
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+          
+        // Dessiner un rectangle blanc qui remplit le canvas
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'white';
 
-      // Dessiner un rectangle blanc qui remplit le canvas
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = 'white';
+        // Dessiner une ligne noire autour du canvas pour la bordure
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 1;
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-      // Dessiner une ligne noire autour du canvas pour la bordure
-      ctx.strokeStyle = 'white';
-      ctx.lineWidth = 1;
-      ctx.strokeRect(0, 0, canvas.width, canvas.height);
+        // Dessiner le milieu du terrain (ligne au milieu)
+        ctx.beginPath();
+        ctx.moveTo(canvas.width / 2, 0);
+        ctx.lineTo(canvas.width / 2, canvas.height);
+        ctx.stroke();
 
-      // Dessiner le milieu du terrain (ligne au milieu)
-      ctx.beginPath();
-      ctx.moveTo(canvas.width / 2, 0);
-      ctx.lineTo(canvas.width / 2, canvas.height);
-      ctx.stroke();
+        const opponent = area.getOpponent();
+        const ballEntity = area.getBall();
+        const racketSize = area.racketSize();
+        const ballSize = area.ballSize();
 
-      const opponent = area.getOpponent();
-      const ballEntity = area.getBall();
-      const racketSize = area.racketSize();
-      const ballSize = area.ballSize();
+        // Espacement des raquettes par rapport aux bords
+        const offsetFromEdge = 10;
 
-      // // Log des valeurs de la raquette du joueur
-      // console.log('Player Y:', player.getLocation().getY());
-      // console.log('Player X:', player.getLocation().getX());
-      // console.log('Racket Width:', racketSize.getWidth());
-      // console.log('Racket Height:', racketSize.getHeight());
+        // Dessiner les raquettes et la balle en utilisant les données de Area
+        ctx.fillRect(offsetFromEdge, racketY, racketSize.getWidth(), racketSize.getHeight());
 
-      // // Log des valeurs de la raquette de l'opposant
-      // console.log('Opponent Y:', opponent.getLocation().getY());
-      // console.log('Opponent X:', opponent.getLocation().getX());
+        ctx.fillRect(
+          canvas.width - racketSize.getWidth() - offsetFromEdge,
+          opponent.getLocation().getY(),
+          racketSize.getWidth(),
+          racketSize.getHeight(),
+        );
 
-      // // Log des valeurs de la balle
-      // console.log('Ball Y:', ballEntity.getLocation().getY());
-      // console.log('Ball X:', ballEntity.getLocation().getX());
-      // console.log('Ball Width:', ballSize.getWidth());
+        // Dessiner le score
+        ctx.font = '42px Arial';
+        ctx.textAlign = 'center';
 
-      // Espacement des raquettes par rapport aux bords
-      const offsetFromEdge = 10;
+        const scoreY = 40; // Position verticale commune pour les scores
 
-      // Dessiner les raquettes et la balle en utilisant les données de Area
-      ctx.fillRect(offsetFromEdge, racketY, racketSize.getWidth(), racketSize.getHeight());
+        ctx.fillText(`0`, canvas.width / 3, scoreY); // Score du joueur
+        ctx.fillText(`0`, (2 * canvas.width) / 3, scoreY); // Score de l'adversaire
 
-      ctx.fillRect(
-        canvas.width - racketSize.getWidth() - offsetFromEdge,
-        opponent.getLocation().getY(),
-        racketSize.getWidth(),
-        racketSize.getHeight(),
-      );
+        ctx.beginPath();
+        ctx.arc(
+          ballEntity.getLocation().getX(),
+          ballEntity.getLocation().getY(),
+          ballSize.getWidth() / 2,
+          0,
+          Math.PI * 2,
+        );
+        ctx.fillStyle = 'white';
+        ctx.fill();
 
-      // Dessiner le score
-      ctx.font = '42px Arial'; // Taille de la police augmentée
-      ctx.textAlign = 'center'; // Alignement au centre
-
-      const scoreY = 40; // Position verticale commune pour les scores
-
-      ctx.fillText(`1`, canvas.width / 3, scoreY); // Score du joueur
-      ctx.fillText(`4`, (2 * canvas.width) / 3, scoreY); // Score de l'adversaire
-
-      ctx.beginPath();
-      ctx.arc(
-        ballEntity.getLocation().getX(),
-        ballEntity.getLocation().getY(),
-        ballSize.getWidth() / 2,
-        0,
-        Math.PI * 2,
-      );
-      ctx.fillStyle = 'white'; // Couleur de la balle
-      ctx.fill();
-
-      requestAnimationFrame(draw);
+        if (canvas != null)
+          sendKeepAlivePacket(racketY);
+        requestAnimationFrame(draw);
+      }
     };
 
-    // Appeler la fonction de dessin
     draw();
 
+    canvas.addEventListener('mousemove', handleMouseMove);
+
     return () => {
-      canvas.removeEventListener('mousemove', handleMouseMove); // Supprimer l'écouteur d'événements
+      canvas.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [socket, racketY]);
+  }, [socket, racketY, isGameStarted]);
 
   return (
     <div>
       <div className="centered-container">
-        <canvas ref={canvasRef} width={800} height={400} />
+        {isGameStarted ? (
+          <canvas ref={canvasRef} width={800} height={400} />
+        ) : (
+          <div>
+            {isChoosingMatchmaking ? (
+              <button onClick={sendMatchmakingPacket}>Start Matchmaking</button>
+            ) : isChoosingDuel ? (
+              <div>
+                <input
+                  type="text"
+                  placeholder="Enter Opponent ID"
+                  value={opponentId !== null ? opponentId.toString() : ''}
+                  onChange={(e): any => { setOpponentId(parseInt(e.target.value))} }
+                />
+                <button onClick={(): void => { 
+                  if (opponentId !== null) {
+                    sendDualPacket(opponentId);
+                  } else {
+                    console.log('Entrez d\'abord l\'ID de l\'opposant.');
+                  }
+                }}>Start Duel</button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={(): void => { setIsChoosingMatchmaking(true)} }>Matchmaking</button>
+                <button onClick={(): void => { setIsChoosingDuel(true)} }>Duel</button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
