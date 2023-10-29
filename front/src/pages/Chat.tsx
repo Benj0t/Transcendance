e06539137dc34278953c7ab4c3fd77720ebc3418
Benjot <bmoreau89000@gmail.com>
2023-10-29 22:55:06 +0100

    Chat new features added: Admin Panel + Working channel selector + Channel members panel + Channel buttons for creation and join

--- front/src/pages/Chat.tsx
+++ front/src/pages/Chat.tsx
@@ -1,61 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ChatWindow from '../components/ChatWindow';
 import { Box } from '@mui/material';
 import ProfileButton from '../components/profileButton';
 import ChatInput from '../components/ChatInput';
 import ChatSelect from '../components/ChatSelect';
+import ButtonCreateChannel from '../components/ButtonCreateChannel';
+import ButtonJoinChannel from '../components/ButtonJoinChannel';
+import MemberList from '../components/MemberList';
+import AdminPanel from '../components/AdminPanel';
 
 const Chat: React.FC = () => {
-  const fakeDatas = [
-    { text: 'Bonjour !', sender: 'John Doe' },
-    { text: 'Salut John, comment ça va ?', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'John Doe' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
-    { text: 'Ça va bien, merci ! Et toi ?', sender: 'John Doe' },
-    { text: 'Je vais bien aussi, merci !', sender: 'Jane Doe' },
-    { text: "As-tu des projets pour aujourd'hui ?", sender: 'John Doe' },
-    { text: 'Oui, je vais travailler sur le projet de site web.', sender: 'Jane Doe' },
-    { text: 'Cela semble génial ! Bonne chance !', sender: 'You' },
-    { text: 'Merci, je vais en avoir besoin !', sender: 'Jane Doe' },
+  const fakeDatas: Record<number, Array<{ text: string; sender: string }>> = {
+    1: [
+      { text: 'Salut !', sender: 'Michel' },
+      { text: 'Salut !', sender: 'You' },
+      { text: 'Salut !', sender: 'Michel' },
+      { text: 'Salut !', sender: 'You' },
+    ],
+    10: [
+      { text: 'Coucou !', sender: 'Sophie' },
+      { text: 'Coucou !', sender: 'You' },
+      { text: 'Coucou !', sender: 'Sophie' },
+      { text: 'Coucou !', sender: 'You' },
+    ],
+    100: [
+      { text: 'Hello !', sender: 'Jack' },
+      { text: 'Hello !', sender: 'You' },
+    ],
+    1000: [
+      { text: 'Bonjour !', sender: 'You' },
+      { text: 'Bonjour !', sender: 'Steve' },
+      { text: 'Bonjour !', sender: 'You' },
+    ],
+  };
+
+  const friends = ['Sophie', 'Marc', 'Mohammed'];
+  const channelsMembers = [
+    { id: 1, names: ['Michel'] },
+    { id: 10, names: ['Sophie'] },
+    { id: 100, names: ['Jack'] },
+    { id: 1000, names: ['Michel', 'Jacky', 'Medhi', 'Elias', 'Daniel'] },
   ];
 
-  const [message, setMessage] = useState('');
-  const [history, setHistory] = useState(fakeDatas);
+  const channels = [
+    { id: 1, name: 'Michel' },
+    { id: 10, name: 'Sophie' },
+    { id: 100, name: 'Jack' },
+    { id: 1000, name: 'Les copains' },
+  ];
 
+  const [selectChannel, setSelectChannel] = useState(1);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [history, setHistory] = useState(fakeDatas[selectChannel]);
+
+  const [message, setMessage] = useState('');
   const onSendMessage = (message: { text: string; sender: string }): void => {
     setHistory([...history, message]);
     console.log(message);
@@ -67,7 +65,12 @@ const Chat: React.FC = () => {
       setMessage('');
     }
   };
-
+  useEffect(() => {
+    const selectedData = fakeDatas[selectChannel];
+    if (selectedData !== undefined) {
+      setHistory(selectedData);
+    }
+  }, [selectChannel]);
   return (
     <Box
       textAlign="right"
@@ -79,37 +82,51 @@ const Chat: React.FC = () => {
         alignItems: 'center',
       }}
     >
-      <Box alignSelf="flex-end">
+      <Box id="avatar" alignSelf="flex-end">
         <ProfileButton />
       </Box>
-      {/* <Box>
-
-      </Box> */}
       <Box
-        display="flex"
+        id="ChatGroup"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 4fr 1fr',
+        }}
         justifyContent="center"
         alignItems="center"
         maxHeight="70%"
-        width="50%"
+        width="100%"
         margin="0"
       >
-        <Box width="100%" maxHeight="70%">
+        <Box id="channelMembersList">
+          <MemberList channelsMembers={channelsMembers} channel={selectChannel} />
+        </Box>
+        <Box id="Chat" width="100%" maxHeight="70%">
           <ChatWindow messages={history} onSendMessage={handleSendMessage} />
           <Box
             style={{
               display: 'grid',
-              gridTemplateColumns: '1fr 4fr',
+              gridTemplateColumns: '13% 10fr',
               width: '100%',
-              height: '100px',
+              height: '80px',
             }}
           >
-            <ChatSelect />
+            <ChatSelect
+              setIsAdmin={setIsAdmin}
+              channelsList={channels}
+              changeChannel={setSelectChannel}
+              channel={selectChannel}
+            />
             <ChatInput
               message={message}
               setMessage={setMessage}
               handleSendMessage={handleSendMessage}
             />
           </Box>
+        </Box>
+        <Box id="channelManagement" display="flex" flexDirection="column" alignItems="end">
+          <ButtonCreateChannel friends={friends} />
+          <ButtonJoinChannel />
+          <AdminPanel isAdmin={isAdmin} />
         </Box>
       </Box>
     </Box>
